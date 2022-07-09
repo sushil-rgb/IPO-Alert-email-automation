ced3c630df97c71c9160708d30f09ff493960ce7
Sushil <107347115+sus0001@users.noreply.github.com>
2022-07-09 22:00:21 +0545

    Update clock.py

--- clock.py
+++ clock.py
@@ -11,7 +11,10 @@ old_update = copy.deepcopy(latest_ipo_news)
 email_receivers = ["rockin_sushil@hotmail.com", "gunz19able@gmail.com", 'sushil.bhandari002@gmail.com']
 sched = BlockingScheduler()
 
-ipo_alert_email.send_email(latest_ipo_news, email_receivers[0], posted_date)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=22.2)
+def scheduled_job():
+    ipo_alert_email.send_email(latest_ipo_news, email_receivers[0], posted_date)
 
 
-ipo_alert_email
+sched.start()
+
